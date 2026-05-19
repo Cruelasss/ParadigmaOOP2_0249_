1867c314be2f16ed483e08f71fe8594fe27b7279
Cruelasss <irula5198@gmail.com>
2026-05-19 10:37:08 +0700

    menambahkan pemanggilan method setsaklarNo untuk mengatur nilai saklar

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -17,5 +17,9 @@ public:
 int main() {
 	remoteLampu lampurumah;
 
+	lampurumah.setsaklarNo(0, "Lampu Teras rumah");
+	lampurumah.setsaklarNo(1, "Lampu Ruang tamu");
+	lampurumah.setsaklarNo(2, "Lampu Kamar tidur");
+	lampurumah.setsaklarNo(3, "Lampu Dapur");
 
 }
